e5f61f13669caf12aa51b511f9ff23b39b77e39b
VictorGlez94 <victorglezguzman@gmail.com>
2024-06-14 00:40:30 +0100

    Profile First Version Done

--- frontend/src/pages/Profile/Profile.jsx
+++ frontend/src/pages/Profile/Profile.jsx
@@ -1,13 +1,28 @@
-import ProfileCard from "../../components/ProfileCard/ProfileCard";
-import "./Profile.css";
+import Container from '@mui/material/Container';
+import Grid from '@mui/material/Grid';
+import ProfileCard from '../../components/ProfileCard/ProfileCard';
+
 
 function Profile() {
+  const userData = {
+    usuario: 'ejemploUsuario',
+    nombre: 'Ejemplo Nombre',
+    fechaNacimiento: '01/01/1990',
+    telefono: '123-456-7890',
+    direccion: 'Ejemplo Dirección',
+    genero: 'Masculino',
+  };
+
   return (
-    <div>
-      <h1>Mi perfil</h1>
-      <ProfileCard />
-    </div>
+    <Container>
+      <Grid container justifyContent="center" spacing={2}>
+        <Grid item xs={12} md={6}>
+          <ProfileCard data={userData} />
+        </Grid>
+      </Grid>
+    </Container>
   );
 }
 
 export default Profile;
+
