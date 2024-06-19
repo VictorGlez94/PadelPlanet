b03e3c98658d9506b54c7cef70b2e0bce18369a4
VictorGlez94 <victorglezguzman@gmail.com>
2024-06-19 22:37:40 +0100

    Favorites done

--- frontend/src/context/CartContext.jsx
+++ frontend/src/context/CartContext.jsx
@@ -1,3 +1,4 @@
+// CartContext.js
 import { createContext, useState, useContext } from "react";
 import PropTypes from "prop-types";
 
@@ -9,21 +10,35 @@ export const useCart = () => {
 
 export const CartProvider = ({ children }) => {
   const [cart, setCart] = useState([]);
+  const [favorites, setFavorites] = useState([]);
+
 
   const addToCart = (product) => {
-    const existingIndex = cart.findIndex((item) => item.id === product.id);
-    if (existingIndex !== -1) {
-      const updatedCart = [...cart];
-      updatedCart[existingIndex].cantidad += 1;
-      setCart(updatedCart);
-    } else {
-      setCart([...cart, { ...product, cantidad: 1 }]);
-    }
+    setCart([...cart, { ...product, cantidad: 1 }]);
   };
 
   const removeFromCart = (productId) => {
     const updatedCart = cart.filter((item) => item.id !== productId);
     setCart(updatedCart);
+  };
+
+  const toggleFavorite = (productId) => {
+    const isFavorite = favorites.some((item) => item.id === productId);
+
+    if (isFavorite) {
+      const updatedFavorites = favorites.filter((item) => item.id !== productId);
+      setFavorites(updatedFavorites);
+    } else {
+      const productToAdd = cart.find((item) => item.id === productId);
+      if (productToAdd) {
+        setFavorites([...favorites, { ...productToAdd, favorite: true }]);
+      } else {
+        // Aquí puedes manejarlo si el producto no está en el carrito pero se marca como favorito
+        // Por ejemplo, podrías hacer una llamada a una API o a un backend para guardar el favorito
+        // Esto depende de cómo manejes la persistencia de datos.
+        console.log("Producto no está en el carrito pero se marca como favorito:", productId);
+      }
+    }
   };
 
   const clearCart = () => {
@@ -34,8 +49,10 @@ export const CartProvider = ({ children }) => {
 
   const value = {
     cart,
+    favorites,
     addToCart,
     removeFromCart,
+    toggleFavorite,
     clearCart,
     cartItemCount,
   };
