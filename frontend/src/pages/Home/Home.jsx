6237fca2463f674a30c37f363cafbb04723c1afd
VictorGlez94 <victorglezguzman@gmail.com>
2024-06-25 19:36:59 +0100

    Home: Sort by date

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,9 +1,8 @@
 import { useState } from 'react';
-import { Container, Typography} from '@mui/material';
+import { Container, Typography } from '@mui/material';
 import SearchBar from '../../components/SearchBar';
 import ProductList from '../../components/ProductList/ProductList';
 import productsData from "../../assets/db/products.json";
-
 import './Home.css';
 import CategoryList from '../../components/CategoryList';
 
@@ -14,14 +13,23 @@ function Home() {
     product.name.toLowerCase().includes(searchTerm.toLowerCase())
   );
 
+  const sortedProducts = filteredProducts.sort((a, b) => {
+    const dateA = new Date(a.created_at);
+    const dateB = new Date(b.created_at);
+    return dateB - dateA; 
+  });
+
   return (
     <Container className="home-container">
-      
-      <Typography variant='h3' color='black' textAlign="center" marginTop={'30px'} marginBottom={'30px'}>¿Qué quieres encontrar?</Typography>
+      <Typography variant='h3' color='black' textAlign="center" marginTop={'30px'} marginBottom={'30px'}>
+        ¿Qué quieres encontrar?
+      </Typography>
       <SearchBar onSearchChange={setSearchTerm} />
-      <CategoryList/>
-      <Typography variant='h4' color='black' textAlign="center" marginTop={'30px'} marginBottom={'30px'}>Productos a la venta</Typography>
-      <ProductList products={filteredProducts} />
+      <CategoryList />
+      <Typography variant='h4' color='black' textAlign="center" marginTop={'30px'} marginBottom={'30px'}>
+        Productos a la venta
+      </Typography>
+      <ProductList products={sortedProducts} />
     </Container>
   );
 }
