6bb4ebbe393d65b9275ba7a36e05254ca2ae9565
VictorGlez94 <victorglezguzman@gmail.com>
2024-06-26 19:40:44 +0100

    Home updated

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,23 +1,59 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { Container, Typography } from '@mui/material';
 import SearchBar from '../../components/SearchBar';
 import ProductList from '../../components/ProductList/ProductList';
-import productsData from "../../assets/db/products.json";
-import './Home.css';
 import CategoryList from '../../components/CategoryList';
+import { api } from '../../services/config';
+import './Home.css';
 
 function Home() {
+  const [products, setProducts] = useState([]);
   const [searchTerm, setSearchTerm] = useState('');
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
-  const filteredProducts = productsData.filter(product =>
+  useEffect(() => {
+    api.get('/product')
+      .then(response => {
+        setProducts(response.data);
+        setLoading(false);
+      })
+      .catch(error => {
+        console.error('Error cargando productos:', error);
+        setError('Ha habido un error cargando productos');
+        setLoading(false);
+      });
+  }, []);
+
+  const filteredProducts = products.filter(product =>
     product.name.toLowerCase().includes(searchTerm.toLowerCase())
   );
 
   const sortedProducts = filteredProducts.sort((a, b) => {
     const dateA = new Date(a.created_at);
     const dateB = new Date(b.created_at);
-    return dateB - dateA; 
+    return dateB - dateA;
   });
+
+  if (loading) {
+    return (
+      <Container className="home-container">
+        <Typography variant='h3' color='black' textAlign="center" marginTop={'30px'} marginBottom={'30px'}>
+          Cargando...
+        </Typography>
+      </Container>
+    );
+  }
+
+  if (error) {
+    return (
+      <Container className="home-container">
+        <Typography variant='h3' color='black' textAlign="center" marginTop={'30px'} marginBottom={'30px'}>
+          {error}
+        </Typography>
+      </Container>
+    );
+  }
 
   return (
     <Container className="home-container">
--- frontend/src/pages/ProductPage/ProductPage.jsx
+++ frontend/src/pages/ProductPage/ProductPage.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/rules-of-hooks */
 import { useState } from "react";
 import { useParams } from "react-router-dom";
 import {
