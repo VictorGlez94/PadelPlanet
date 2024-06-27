7213da94df06391d2af3fab5ece61988d32cc077
VictorGlez94 <victorglezguzman@gmail.com>
2024-06-27 22:19:16 +0100

    Favorites Fixed V1

--- frontend/src/pages/Favorites/Favorites.jsx
+++ frontend/src/pages/Favorites/Favorites.jsx
@@ -1,16 +1,19 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable no-unused-vars */
 import { useEffect, useState } from "react";
-import ProductList from "../../components/ProductList/ProductList";
 import { Box, Typography, Button } from "@mui/material";
 import { Link } from "react-router-dom";
 import { api } from "../../services/config";
 import { useAuth } from "../../context/AuthContext";
+import ProductList from '../../components/ProductList/ProductList';
 
 function Favorites() {
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
   const [likes, setLikes] = useState([]);
   const [userFavorites, setUserFavorites] = useState([]);
+  const [products, setProducts] = useState([]);
+  const [users, setUsers] = useState([]);
 
   const { userId } = useAuth();
 
@@ -22,16 +25,18 @@ function Favorites() {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const [likesResponse] = await Promise.all([
-          await api.get("like/", { headers: headers }),
+        const [likesResponse, productsResponse, usersResponse] = await Promise.all([
+          api.get("like/", { headers }),
+          api.get("product/", { headers }),
+          api.get("user/", { headers }),
         ]);
-        setLikes(likesResponse.data);
-        console.log(likes)
-        const filteredFavorites = likes.filter((like) => like.user_id === 8);
-        setUserFavorites(filteredFavorites);
-        console.log(filteredFavorites)
-        setLoading(false);
 
+        setLikes(likesResponse.data);
+        const filteredFavorites = likesResponse.data.filter((like) => like.user_id === userId);
+        setUserFavorites(filteredFavorites);
+        setProducts(productsResponse.data);
+        setUsers(usersResponse.data);
+        setLoading(false);
       } catch (error) {
         console.error("Ha habido un error buscando los productos favoritos", error);
         setError("Ha habido un error buscando los productos favoritos");
@@ -40,11 +45,31 @@ function Favorites() {
     };
 
     fetchData();
-  }, []);
+  }, [userId]);
 
+  const getFavoriteProductsDetails = () => {
+    const favoriteProducts = userFavorites.map((favorite) => {
+      const product = products.find((p) => p.id === favorite.product_id);
+      return product;
+    });
 
+    return favoriteProducts;
+  };
 
-  
+  const joinDataForSales = (favoriteProducts, users) => {
+    return favoriteProducts.map((product) => {
+      const user = users.find((user) => user.id === product.seller_id);
+      return {
+        ...product,
+        sellerName: user ? user.username : "Desconocido",
+      };
+    });
+  };
+
+  const favoriteProducts = getFavoriteProductsDetails();
+  const favoriteProductsJoined = joinDataForSales(favoriteProducts, users);
+
+  console.log(favoriteProductsJoined)
 
   return (
     <Box sx={{ flexGrow: 1, ml: "80px", mr: "80px", mb: "40px" }}>
@@ -67,8 +92,8 @@ function Favorites() {
         </Box>
       ) : (
         <Box sx={{ textAlign: "center" }}>
-        <Typography variant="h4">Tus productos favoritos:</Typography>
-        <ProductList products={userFavorites} />
+          <Typography variant="h4">Tus productos favoritos:</Typography>
+          <ProductList products={favoriteProductsJoined} />
         </Box>
       )}
     </Box>
